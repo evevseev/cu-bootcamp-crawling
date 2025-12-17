229b4ec85141d97cdd46171bc4b7c8a37f85e8d5
Evgeny Evseev <evgeny.evseev@vk.team>
2025-12-17 19:08:19 +0300

    init

---
+++ cu_crawling/cu_crawling/spiders/merchantpoint_ru.py
@@ -0,0 +1,45 @@
+import scrapy
+import scrapy.http
+
+
+class MerchantpointRuSpider(scrapy.Spider):
+    name = "merchantpoint_ru"
+    allowed_domains = ["merchantpoint.ru"]
+    start_urls = ["https://merchantpoint.ru/brand/4390"]
+
+    def parse(self, response: scrapy.http.Response):
+        ord_desc = (
+            response.xpath("//div[contains(@class, 'description_brand')]/text()")
+            .get()
+            .strip()
+        )  # type: ignore
+        merchant_urls = response.xpath(
+            "//table[@class='finance-table']//a/@href"
+        ).getall()
+
+        yield from response.follow_all(
+            urls=merchant_urls,  # type: ignore
+            cb_kwargs={"org_desc": ord_desc},
+            callback=self.parse_merchant,
+        )
+
+    # merchant_name - //h1/text()
+    # mcc - //section[@id='description']//p/a/text()
+    # address - //div//p[contains(., 'Адрес')]/text()
+    # geo_coordinates - //div//p[contains(., 'Гео')]/text()
+    # org_name - //p/a[contains(@href,'brand')]/text()
+
+    # org_description - //div[contains(@class, 'description_brand')]/text()
+
+    def parse_merchant(self, response: scrapy.http.Response, org_desc: str):
+        return {
+            "org_name": response.xpath("//p/a[contains(@href,'brand')]/text()").get(),
+            "org_desc": org_desc,
+            "merchant_name": response.xpath("//h1/text()").get(),
+            "mcc": response.xpath("//section[@id='description']//p/a/text()").get(),
+            "address": response.xpath("//div//p[contains(., 'Адрес')]/text()").get(),
+            "geo_coordinates": response.xpath(
+                "//div//p[contains(., 'Гео')]/text()"
+            ).get(),
+            "source_url": response.url,
+        }
